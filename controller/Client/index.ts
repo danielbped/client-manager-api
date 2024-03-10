0d9249f0101a81902d9b2650077b9a09c4b80132
danielbped <danielbped@gmail.com>
2024-03-09 23:32:14 -0300

    feat: adiciona rota para criação de um novo cliente e uma função para buscar cliente por email

--- controller/Client/index.ts
+++ controller/Client/index.ts
@@ -1,24 +1,38 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes'
-import Client from '../../model/Client'
+import ClientService from '../../service/Client';
 
 const router = Router({ mergeParams: true });
 
 router.get('/', async (_req, res) => {
   try {
-    const result = await Client.list();
+    const result = await ClientService.list();
 
     return res.status(StatusCodes.OK).json(result);
   } catch (err: any) {
     console.error(err.message);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       message: err.message || 'Internal server error'
-    })
-  }
+    });
+  };
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, phone, email } = req.body;
+    const result = await ClientService.create({ name, phone, email });
+
+    return res.status(StatusCodes.CREATED).json(result);
+  } catch (err: any) {
+    console.error(err);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: err.message || 'Internal server error'
+    });
+  };
 })
 
 const client = (root: Router) => {
   root.use('/client', router)
-}
+};
 
 export { client };
--- model/Client/index.ts
+++ model/Client/index.ts
@@ -1,19 +1,49 @@
-import { client } from "../../database"
-import { uuid } from "uuidv4"
+import { client } from "../../database";
+import { uuid } from "uuidv4";
+import { Client } from "../../entities/Client";
 
 export default {
   async list() {
-    const result = await client.query('SELECT * FROM public.clients');
-
-    return {
-      result: result.rows,
-      count: result.rowCount
+    try {
+      const result = await client.query('SELECT * FROM public.clients');
+  
+      return {
+        result: result.rows,
+        count: result.rowCount
+      };
+    } catch (err: any) {
+      console.error(err);
+      throw new Error(err);
     }
   },
-  async create(name: string, email: string, phone: string) {
-    return client.query(`
-    INSERT INTO public.clients(
-      id, name, email, phone)
-      VALUES (${uuid()}, ${name}, ${email}, ${phone});`)
+  async create(data: Partial<Client>) {
+    try {
+      await client.query(`
+        INSERT INTO public.clients(
+          id, name, email, phone)
+          VALUES ('${uuid()}', '${data.name}', '${data.email}', '${data.phone}');
+        `);
+      
+      return {
+        name: data.name,
+        email: data.email,
+        phone: data.phone,
+      };
+    } catch (err: any) {
+      console.error(err);
+      throw new Error(err);
+    };
+  },
+  async findByEmail(email: string) {
+    try {
+      const result = await client.query(`
+        SELECT * FROM public.clients where email = '${email}'
+      `);
+
+      return result.rows[0];
+    } catch (err: any) {
+      console.error(err);
+      throw new Error(err);
+    };
   }
-}
+};
