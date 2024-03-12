077c8e49fe8994b25b8837865a569472c6146166
danielbped <danielbped@gmail.com>
2024-03-12 01:15:58 -0300

    chore: adiciona cors para receber requisicao de localhost

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import { json } from 'body-parser';
 import * as db from './database';
 import router from './controller';
+import cors from 'cors';
 
 const PORT = 3000;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}))
 app.use(router);
 
-db.connect();
 
+db.connect();
 app.get('/health', async (_req: any, res: any) => {
   res.status(200).send({ message: 'OK' });
 })
